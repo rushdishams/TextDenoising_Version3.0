4702c68acf15882874af4a3457b38c7b1534aa49
rushdishams <rushdecoder@yahoo.com>
2015-02-03 14:33:25 -0500

    Test

--- src/ca/uwo/csd/cogeng/textdenoising/Directory.java
+++ src/ca/uwo/csd/cogeng/textdenoising/Directory.java
@@ -1,6 +1,6 @@
 package ca.uwo.csd.cogeng.textdenoising;
 /**
- * Class to represent a directory
+ * BAAAAAAAAAAAClass to represent a directory
  * @author Rushdi Shams
  * @version 3.0 (Major optimization applied January 20 2015
  *
